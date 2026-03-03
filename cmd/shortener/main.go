4f7b530c3a284aef75ae6138d524aed7a4c6f22c
HimmelSpark <adam404pet@gmail.com>
2026-03-03 11:38:15 +0300

    switched from http default mux to chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,10 +7,12 @@ import (
 	"github.com/HimmelSpark/go-musthave-shortener.git/internal/middleware"
 	"github.com/HimmelSpark/go-musthave-shortener.git/internal/repository"
 	"github.com/HimmelSpark/go-musthave-shortener.git/internal/service"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
+	r.Use(middleware.LoggingMiddleware)
 
 	//dbConn := db.GetDBConnection()
 	//urlRepo := repository.NewURLRepository(dbConn)
@@ -19,12 +21,10 @@ func main() {
 	shortenerService := service.NewShortenerService(urlRepo)
 	shortenerHandler := handler.NewShortenerHandler(shortenerService)
 
-	mux.HandleFunc("POST /", shortenerHandler.ShortenURL)
-	mux.HandleFunc("GET /{urlId}", shortenerHandler.GetRedirectURL)
+	r.Post("/", shortenerHandler.ShortenURL)
+	r.Get("/{urlId}", shortenerHandler.GetRedirectURL)
 
-	handlerChain := middleware.LoggingMiddleware(mux)
-
-	if err := http.ListenAndServe("localhost:8080", handlerChain); err != nil {
+	if err := http.ListenAndServe("localhost:8080", r); err != nil {
 		panic(err)
 	}
 }
