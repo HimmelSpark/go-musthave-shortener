987a85dab9862d918574edff763bd64bbe77ef00
HimmelSpark <adam404pet@gmail.com>
2026-03-16 15:19:46 +0300

    - added env param based configs - fix go version for shortenertest

--- internal/config/shortener/shortener_service_config.go
+++ internal/config/shortener/shortener_service_config.go
@@ -1,13 +1,22 @@
 package shortener
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/caarlos0/env/v6"
+)
 
 type ServiceConfig struct {
-	BaseURL *string
+	BaseURL *string `env:"BASE_URL,required"`
 }
 
 func Init() *ServiceConfig {
+	cmdBaseURL := flag.String("b", "http://localhost:8080", "server server port")
+	var cfg ServiceConfig
+	if err := env.Parse(&cfg); err == nil {
+		return &cfg
+	}
 	return &ServiceConfig{
-		BaseURL: flag.String("b", "http://localhost:8080", "server server port"),
+		BaseURL: cmdBaseURL,
 	}
 }
