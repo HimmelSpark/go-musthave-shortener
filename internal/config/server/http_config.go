55d9bbf0a9bcc0e9552078689edfa2c082935690
HimmelSpark <adam404pet@gmail.com>
2026-03-16 15:19:46 +0300

    - added env param based configs - fix go version for shortenertest

--- internal/config/server/http_config.go
+++ internal/config/server/http_config.go
@@ -1,13 +1,22 @@
 package server
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/caarlos0/env/v6"
+)
 
 type Config struct {
-	ServerAddress *string
+	ServerAddress *string `env:"SERVER_ADDRESS,required"`
 }
 
 func Init() *Config {
+	cmdServerAddress := flag.String("a", ":8080", "Server address")
+	var config Config
+	if err := env.Parse(&config); err == nil {
+		return &config
+	}
 	return &Config{
-		ServerAddress: flag.String("a", "localhost:8080", "HTTP server address"),
+		ServerAddress: cmdServerAddress,
 	}
 }
