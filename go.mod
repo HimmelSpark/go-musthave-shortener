ea4f650986056b0c75a8116a6f8f549b4b5d15b2
HimmelSpark <adam404pet@gmail.com>
2026-03-23 21:21:21 +0300

    implemented middleware logging

--- go.mod
+++ go.mod
@@ -11,9 +11,13 @@ require (
 	github.com/jackc/pgx/v5 v5.8.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/matoous/go-nanoid/v2 v2.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
 	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/middleware/logging-middleware.go
+++ internal/middleware/logging-middleware.go
@@ -1,20 +1,50 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/rs/zerolog/log"
 )
+
+type responseData struct {
+	status int
+	size   int
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := r.ParseForm(); err != nil {
-			fmt.Println("Error parsing request data", err)
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
-		}
-		for k, v := range r.Form {
-			fmt.Println("k:", k, "v:", v)
-		}
-		next.ServeHTTP(w, r)
+		start := time.Now()
+
+		rd := &responseData{status: http.StatusOK}
+		lw := &loggingResponseWriter{ResponseWriter: w, responseData: rd}
+
+		next.ServeHTTP(lw, r)
+
+		duration := time.Since(start)
+
+		log.Info().
+			Str("uri", r.RequestURI).
+			Str("method", r.Method).
+			Dur("duration", duration).
+			Int("status", rd.status).
+			Int("size", rd.size).
+			Msg("request completed")
 	})
 }
